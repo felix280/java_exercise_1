55dd9b41e43404563c7f60c19cfd0e1737e9fa8d
Rousseau Félix <ferousseau@et.esiea.fr>
2021-11-29 15:26:10 +0100

    Launcher2.0

--- src/Launcher.java
+++ src/Launcher.java
@@ -2,15 +2,16 @@ import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) {
-        System.out.print( "Veuillez saisir un premier entier : " );
+        System.out.print( "Bonjour vueillez entrer une commande : " );
         Scanner scanner = new Scanner( System.in );
         String a = scanner.nextLine();
-
-        if(a.equals("quit")){
-
-        }else{
-            System.out.print( "Unknown command" );
-        }
+        while(true)
+            if(a.equals("quit")){
+                return;
+            }else{
+                System.out.print( "Unknown command\n" );
+                a = scanner.nextLine();
+            }
 
     }
 }
