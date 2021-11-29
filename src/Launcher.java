4f0f4311ffcd168679eaec17a2d71755db2f317b
Rousseau Félix <ferousseau@et.esiea.fr>
2021-11-29 15:40:48 +0100

    Fibo

--- src/Launcher.java
+++ src/Launcher.java
@@ -8,7 +8,29 @@ public class Launcher {
         while(true)
             if(a.equals("quit")){
                 return;
-            }else{
+            }
+            else if(a.equals("fibo")){
+                System.out.printf( "Entrez un nombre :");
+                int number = scanner.nextInt();
+
+                int fn1 =0, fn2=1;
+                int result =0;
+                if(number<2){
+                    System.out.printf( "Resultat : %d\n", number );
+                    return;
+                }else{
+                    for (int i=1; i<number; i++)
+                    {
+                        result= fn1 + fn2;
+                        fn1 =fn2;
+                        fn2= result;
+                    }
+                    System.out.printf( "Resultat : %d\n", result );
+                    return ;
+                }
+
+            }
+            else{
                 System.out.print( "Unknown command\n" );
                 a = scanner.nextLine();
             }
